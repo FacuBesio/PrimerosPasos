d199a8a9aa17a5ca257d2931c5739976704b03bc
Facundo Besio <facubesio.08@gmail.com>
2024-06-26 15:07:49 -0300

    Modularización de Categories.jsx

--- frontend/src/components/Categories/Categories.jsx
+++ frontend/src/components/Categories/Categories.jsx
@@ -1,29 +1,12 @@
-import { useContext, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { CategoriesContext } from "../../context/index";
-import getCategories from "../../utils/categories/getCategories";
-import { handlerClickCategories } from "../../utils/filter/filterHandlers";
+import { Link } from "react-router-dom";
 import bathImage from "../../assets/baño.png";
 import babyBagImage from "../../assets/babybag.jpg";
 import clothesImage from "../../assets/ropa.png";
 import shoesImage from "../../assets/shoes.png";
 
-
 const Categories = () => {
-  const {
-    allCategories,
-    setAllCategories,
-    setFilterCategories,
-    setCategoryTag,
-  } = useContext(CategoriesContext);
 
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    getCategories(setAllCategories);
-  }, []);
-
-  const categoriesImages = [
+  const selectedCategories = [
     {
       name: "Baño",
       image: bathImage,
@@ -46,41 +29,25 @@ const Categories = () => {
     },
   ];
 
-  const categoriesWithImages = allCategories?.categories
-    ?.slice(0, 4)
-    .map((cat, index) => ({
-      ...cat,
-      image: categoriesImages[index]?.image,
-      description: categoriesImages[index]?.description,
-    }));
-
   return (
     <section className="sm:grid grid-rows-2 grid-cols-2  bg-categories">
-      {categoriesWithImages?.map((cat) => (
+      {selectedCategories.map((category) => (
         <Link
-          to={`/shop/categories/${cat.name}`}
-          key={cat.name}
-          onClick={() =>
-            handlerClickCategories(
-              navigate,
-              setFilterCategories,
-              setCategoryTag,
-              cat
-            )
-          }
+          to={`/shop/categories/${category.name}`}
+          key={category.name}
           className="max-h-[360px] flex border-b-red-100 border-b-2 cursor-pointer"
         >
           <img
             className="w-1/2 border-l-red-200 border-l-1 object-cover max-w-[320px]"
-            src={cat.image}
-            alt="Imagen categorias"
+            src={category.image}
+            alt={`Imagen de ${category.name}`}
           />
           <div className="flex gap-4 flex-col justify-center items-center w-full border-l-red-200 border-l-1">
             <h2 className="text-[rgb(90,91,90)] md:text-3xl text-center">
-              {cat.name}
+              {category.name}
             </h2>
             <div className="text-[#Dbb1bc] md:text-xl lg:min-w-[148px] text-center border p-1 mx-4 rounded-md border-red-200 bg-slate-50">
-              {cat.description}
+              {category.description}
             </div>
           </div>
         </Link>
